8af29fc684b89ead07be8d3521aaaae4eda0a7e3
Kelvin Souza <kelvinsouza2017@hotmail.com>
2023-03-10 19:21:24 -0300

    Desenvolvimento de JavaScript

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+// FUNÇÃO DE REPRODUZIR O SOM
+function tocarSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+//REFERÊNCIA CONSTANTE
+const listaDeBotoes = document.querySelectorAll('.botao');
+
+// LÓGICA DE ENQUANTO / REFERÊNCIA E VALOR INICIAL / ENQUANTO A REFERÊNCIA FOR MENOR QUE LENGTH / OPERAÇÃO (contador = contador + 1)
+for (let contador = 0; contador < listaDeBotoes.length; contador++) {
+    
+    //SELECIONAR O ELEMENTO DO ÍNDICE
+    const botao = listaDeBotoes[contador];
+    const instrumento = botao.classList[1];
+    const idAudio = `#audio_${instrumento}`;
+    
+    console.log(idAudio)
+    //REFERÊNCIA (lista) SELECIONANDO O ELEMENTO "x" DO ÍNDICE / EXECUTAR FUNÇÃO / ABERTURA DE FUNÇÃO ANÔNIMA / EXECUÇÃO DE FUNÇÃO (tocarSom)
+    botao.onclick = function () {
+        tocarSom(idAudio);
+    }
+}
